534f30d4a72a5f39d867ad8176e8181d4f2a7a44
Jakhart <v.richard@estia.local>
2017-10-16 17:55:52 +0100

    work with optionnal array

--- main.cpp
+++ main.cpp
@@ -7,21 +7,36 @@
 using namespace std;
 
 int main()
-{
-	double T[] = {4, 2, 6, 1, 9};
+{	
+	int n;	
+	cout << "size of the Array:" << endl;
+	cin >> n;
+	cout << "Fill the Array:" << endl;
+	double T[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> T[i];
+	};
+	//double T[] = {4, 2, 6, 1, 9};
 	cout << "avant le tri : " << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << T[i] << endl;
 	}
-
-	tri_insertion(T, 5);
+	int I[n];
+	tri_insertion(T, n, I);
 	//T = quickSort(T, 0, 4);
 
 	cout << "apres le tri : " << endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << T[i] << endl;
+	};
+
+	cout << "ordre initial : " << endl;	
+	for (int i = 0; i < n; i++)
+	{
+		cout << I[i] << endl;
 	};
 
 	return 0;
--- sorting.cpp
+++ sorting.cpp
@@ -5,27 +5,41 @@
 
 /// \brief Une methode
 /**This part regroup the two different methods we use in order to sort a sequence of value.*/
-void tri_insertion(double *T, int n)  // n taille de la chaine
+void tri_insertion(double *T, int n, int *I) // n taille de la chaine
 {
+	for (int k = 0; k < n; k++)
+		I[k] = k+1;
 	int i, j;
 	for (i = 1; i < n; i++)
 	{
-		int x = T[i];  // x est l'element selectionner
-		for (j = i; j > 0 && T[j - 1] > x; j--) T[j] = T[j - 1];
+		int x = T[i];
+		int y = I[i];
+		for (j = i; j > 0 && T[j - 1] > x; j--)
+		{
+			T[j] = T[j - 1];
+			I[j] = I[j - 1];
+		}
+
+		I[j] = y;
 		T[j] = x;
-	};
+	}
 };
 
-void quickSort(double *T, int left, int right) {
+void quickSort(double *T, int left, int right)
+{
 	int i = left, j = right;
 	int tmp;
 	int pivot = T[(left + right) / 2];
 
 	//partition
-	while (i <= j) {
-		while (T[i] < pivot) i++;
-		while (T[j] > pivot) j--;
-		if (i <= j) {
+	while (i <= j)
+	{
+		while (T[i] < pivot)
+			i++;
+		while (T[j] > pivot)
+			j--;
+		if (i <= j)
+		{
 			tmp = T[i];
 			T[i] = T[j];
 			T[j] = tmp;
@@ -35,6 +49,8 @@ void quickSort(double *T, int left, int right) {
 	};
 
 	//recursion
-	if (left < j) quickSort(T, left, j);
-	if (i < right) quickSort(T, i, right);
+	if (left < j)
+		quickSort(T, left, j);
+	if (i < right)
+		quickSort(T, i, right);
 }
